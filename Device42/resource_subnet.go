0bc0b53a7ba8871a4713c070a7e8ab6bd3251203
Charles GERARD-LE METAYER <33660847+gerardlemetayerc@users.noreply.github.com>
2023-04-26 09:24:24 +0200

    Subnet - Implementation of Create / Update / Delete / Read
    Implementation for subnet in D42 : creation, deletion, read and update works.
    Issue on vrfGroup field.

--- Device42/resource_subnet.go
+++ Device42/resource_subnet.go
@@ -9,21 +9,44 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+
+type apiSubnetReadResponse struct {
+	ID                      int64         `json:"id"`
+	Network                 string 	      `json:"network"`	
+	Mask_Bits               int64         `json:"mask_bits"`
+	vrfGroup                string        `json:"vrf_group"`
+	Name                    string        `json:"name"`
+}
+
+
+
 func resourceD42Subnet() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceDevice42SubnetCreate,
+		Read:   resourceDevice42SubnetRead,
+		Update: resourceDevice42SubnetUpdate,
+		Delete: resourceDevice42SubnetDelete,
 
 		Schema: map[string]*schema.Schema{
-			"name": {
+			"network": {
 				Type:        schema.TypeString,
 				ForceNew:    true,
+				Required:    true,
+				Description: "Network of the subnet. Required for creation, cannot be modified after subnet creation.",
+			},
+			"mask_bits": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Mask bits of the subnet. Required for creation, can be modified after subnet creation.",
+			},
+			"name": {
+				Type:        schema.TypeString,
 				Required:    true,
 				Description: "The hostname of the device.",
 			},
 			"vrf_group": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				Default:     "d42null",
 				Description: "Subnet VRF Group",
 			},
 			"parent_subnet_id": {
@@ -35,10 +58,6 @@ func resourceD42Subnet() *schema.Resource {
 				Type:        schema.TypeMap,
 				Optional:    true,
 				Description: "Any custom fields that will be used in device42.",
-			},
-			"mask_bits": {
-				Type:     schema.TypeMap,
-				Optional: true,
 			},
 			"description": {
 				Type:     schema.TypeMap,
@@ -67,14 +86,16 @@ func resourceD42Subnet() *schema.Resource {
 func resourceDevice42SubnetCreate(d *schema.ResourceData, m interface{}) error {
 	client := m.(*resty.Client)
 	name := d.Get("name").(string)
-	deviceType := d.Get("type").(string)
-	serviceLevel := d.Get("service_level").(string)
+	network := d.Get("network").(string)
+	maskBits := d.Get("mask_bits").(string)
+	vrfGroup := d.Get("vrf_group").(string)
 
 	resp, err := client.R().
 		SetFormData(map[string]string{
-			"name":          name,
-			"type":          deviceType,
-			"service_level": serviceLevel,
+			"name":      name,
+			"network":   network,
+			"mask_bits": maskBits,
+			"vrfGroup" : vrfGroup,
 		}).
 		SetResult(apiResponse{}).
 		Post("/1.0/subnets/")
@@ -92,7 +113,79 @@ func resourceDevice42SubnetCreate(d *schema.ResourceData, m interface{}) error {
 	log.Printf("[DEBUG] Result: %#v", r)
 	id := int(r.Msg[1].(float64))
 
+	// Set ID after subnet creation
 	d.SetId(strconv.Itoa(id))
 
 	return resourceDevice42DeviceRead(d, m)
+}
+
+func resourceDevice42SubnetRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*resty.Client)
+	resp, err := client.R().
+		SetResult(apiSubnetReadResponse{}).
+		Get(fmt.Sprintf("/1.0/subnets/%s/", d.Id()))
+
+	if err != nil {
+		log.Printf("[WARN] No subnet found: %s", d.Id())
+		d.SetId("")
+		return err
+	}
+
+	r := resp.Result().(*apiSubnetReadResponse)
+
+	d.Set("name", r.Name)
+	d.Set("network", r.Network)
+	d.Set("mask_bits", r.Mask_Bits)
+	d.Set("vrf_group", r.vrfGroup)
+
+	return nil
+}
+
+func resourceDevice42SubnetUpdate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*resty.Client)
+
+	if(d.HasChange("name") || d.HasChange("mask_bits") || d.HasChange("vrf_group")){
+		name := d.Get("name").(string)
+		maskBits := d.Get("mask_bits").(string)
+		vrfGroup := d.Get("vrf_group").(string)
+		url := fmt.Sprintf("/1.0/subnets/%s/", d.Id())
+
+
+		resp, err := client.R().
+			SetFormData(map[string]string{
+				"name":        name,
+				"mask_bits":   maskBits,
+				"vrf_group":   vrfGroup,
+			}).
+			SetResult(apiResponse{}).
+			Put(url)
+
+		if err != nil {
+			return err
+		}
+		r := resp.Result().(*apiResponse)
+		log.Printf("[DEBUG] Result: %#v", r)
+	}
+
+
+	return resourceDevice42SubnetRead(d, m)
+}
+
+func resourceDevice42SubnetDelete(d *schema.ResourceData, m interface{}) error {
+	client := m.(*resty.Client)
+	log.Printf("Deleting subnet %s (UUID: %s)", d.Get("name"), d.Id())
+
+	url := fmt.Sprintf("/1.0/subnets/%s/", d.Id())
+
+	resp, err := client.R().
+		SetResult(apiResponse{}).
+		Delete(url)
+
+	if err != nil {
+		return err
+	}
+
+	r := resp.Result().(*apiResponse)
+	log.Printf("[DEBUG] Result: %#v", r)
+	return nil
 }
