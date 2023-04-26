37da962ac430e705da82ba5a1f59a9a63bc67ad1
Charles GERARD-LE METAYER <33660847+gerardlemetayerc@users.noreply.github.com>
2023-04-26 14:14:29 +0200

    Quick patch - Correction of "boulette"
    Missclick

--- Device42/provider.go
+++ Device42/provider.go
@@ -1,191 +1,93 @@
 package device42
 
 import (
+	"crypto/tls"
 	"fmt"
-	"log"
-	"strconv"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+// Environment variables the provider recognizes for configuration
+const (
+	// Environment variable to configure the device42 api host
+	HostEnv string = "d42_host"
+	// Environment variable to configure the device42 api username attribute
+	UsernameEnv string = "d42_username"
+	// Environment variable to configure the device42 api password attribute
+	PasswordEnv string = "d42_password"
+)
 
-type apiSubnetReadResponse struct {
-	ID                      int64         `json:"id"`
-	Network                 string 	      `json:"network"`	
-	Mask_Bits               int64         `json:"mask_bits"`
-	vrfGroup                string        `json:"vrf_group"`
-	Name                    string        `json:"name"`
-}
-
-
-
-func resourceD42Subnet() *schema.Resource {
-	return &schema.Resource{
-		Create: resourceDevice42SubnetCreate,
-		Read:   resourceDevice42SubnetRead,
-		Update: resourceDevice42SubnetUpdate,
-		Delete: resourceDevice42SubnetDelete,
-
+// Provider -- main device42 provider structure
+func Provider() *schema.Provider {
+	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"network": {
-				Type:        schema.TypeString,
-				ForceNew:    true,
-				Required:    true,
-				Description: "Network of the subnet. Required for creation, cannot be modified after subnet creation.",
+			// -- API Interaction Definitions --
+			"d42_host": {
+				Type:     schema.TypeString,
+				Required: true,
+				DefaultFunc: schema.EnvDefaultFunc(
+					HostEnv,
+					"",
+				),
+				Description: "The device42 server to interact with.",
 			},
-			"mask_bits": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Mask bits of the subnet. Required for creation, can be modified after subnet creation.",
-			},
-			"name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "The hostname of the device.",
-			},
-			"vrf_group": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "Subnet VRF Group",
-			},
-			"parent_subnet_id": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "Parent subnet id",
-			},
-			"customer": {
-				Type:        schema.TypeMap,
-				Optional:    true,
-				Description: "Any custom fields that will be used in device42.",
-			},
-			"description": {
-				Type:     schema.TypeMap,
+			"d42_password": {
+				Type:     schema.TypeString,
 				Optional: true,
+				DefaultFunc: schema.EnvDefaultFunc(
+					PasswordEnv,
+					"",
+				),
+				Description: "The password to authenticate with Device42.",
 			},
-			"gateway": {
-				Type:     schema.TypeMap,
+			"d42_username": {
+				Type:     schema.TypeString,
 				Optional: true,
+				DefaultFunc: schema.EnvDefaultFunc(
+					UsernameEnv,
+					"",
+				),
+				Description: "The username to authenticate with Device42.",
 			},
-			"service_level": {
-				Type:     schema.TypeMap,
+			"d42_tls_unsecure": {
+				Type:     schema.TypeBool,
 				Optional: true,
-			},
-			"category": {
-				Type:     schema.TypeMap,
-				Optional: true,
-			},
-			"vlan": {
-				Type:     schema.TypeMap,
-				Optional: true,
+				Default:  false,
+				Description: "Whether to perform TLS cert verification on the server's certificate. " +
+					"Defaults to `false`.",
 			},
 		},
+		ResourcesMap: map[string]*schema.Resource{
+			"device42_device": resourceD42Device(),
+			"device42_subnet": resourceD42Subnet(),
+		},
+		ConfigureFunc: providerConfigure,
 	}
 }
 
-func resourceDevice42SubnetCreate(d *schema.ResourceData, m interface{}) error {
-	client := m.(*resty.Client)
-	name := d.Get("name").(string)
-	network := d.Get("network").(string)
-	maskBits := d.Get("mask_bits").(string)
-	vrfGroup := d.Get("vrf_group").(string)
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	host := d.Get("d42_host").(string)
+	username := d.Get("d42_username").(string)
+	password := d.Get("d42_password").(string)
+	tlsInsecure := d.Get("d42_tls_unsecure").(bool)
 
-	resp, err := client.R().
-		SetFormData(map[string]string{
-			"name":      name,
-			"network":   network,
-			"mask_bits": maskBits,
-			"vrfGroup" : vrfGroup,
-		}).
-		SetResult(apiResponse{}).
-		Post("/1.0/subnets/")
-
-	if err != nil {
-		return err
+	if host == "" {
+		return nil, fmt.Errorf("no Device42 host was provided")
 	}
 
-	r := resp.Result().(*apiResponse)
-
-	if r.Code != 0 {
-		return fmt.Errorf("API returned code %d", r.Code)
+	if username == "" {
+		return nil, fmt.Errorf("no username was provided")
 	}
 
-	log.Printf("[DEBUG] Result: %#v", r)
-	id := int(r.Msg[1].(float64))
-
-	// Set ID after subnet creation
-	d.SetId(strconv.Itoa(id))
-
-	return resourceDevice42DeviceRead(d, m)
-}
-
-func resourceDevice42SubnetRead(d *schema.ResourceData, m interface{}) error {
-	client := m.(*resty.Client)
-	resp, err := client.R().
-		SetResult(apiSubnetReadResponse{}).
-		Get(fmt.Sprintf("/1.0/subnets/%s/", d.Id()))
-
-	if err != nil {
-		log.Printf("[WARN] No subnet found: %s", d.Id())
-		d.SetId("")
-		return err
+	if password == "" {
+		return nil, fmt.Errorf("no password was provided")
 	}
 
-	r := resp.Result().(*apiSubnetReadResponse)
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: tlsInsecure})
+	client.SetBaseURL(fmt.Sprintf("https://%s/api", host))
+	client.SetBasicAuth(username, password)
 
-	d.Set("name", r.Name)
-	d.Set("network", r.Network)
-	d.Set("mask_bits", r.Mask_Bits)
-	d.Set("vrf_group", r.vrfGroup)
-
-	return nil
-}
-
-func resourceDevice42SubnetUpdate(d *schema.ResourceData, m interface{}) error {
-	client := m.(*resty.Client)
-
-	if(d.HasChange("name") || d.HasChange("mask_bits") || d.HasChange("vrf_group")){
-		name := d.Get("name").(string)
-		maskBits := d.Get("mask_bits").(string)
-		vrfGroup := d.Get("vrf_group").(string)
-		url := fmt.Sprintf("/1.0/subnets/%s/", d.Id())
-
-
-		resp, err := client.R().
-			SetFormData(map[string]string{
-				"name":        name,
-				"mask_bits":   maskBits,
-				"vrf_group":   vrfGroup,
-			}).
-			SetResult(apiResponse{}).
-			Put(url)
-
-		if err != nil {
-			return err
-		}
-		r := resp.Result().(*apiResponse)
-		log.Printf("[DEBUG] Result: %#v", r)
-	}
-
-
-	return resourceDevice42SubnetRead(d, m)
-}
-
-func resourceDevice42SubnetDelete(d *schema.ResourceData, m interface{}) error {
-	client := m.(*resty.Client)
-	log.Printf("Deleting subnet %s (UUID: %s)", d.Get("name"), d.Id())
-
-	url := fmt.Sprintf("/1.0/subnets/%s/", d.Id())
-
-	resp, err := client.R().
-		SetResult(apiResponse{}).
-		Delete(url)
-
-	if err != nil {
-		return err
-	}
-
-	r := resp.Result().(*apiResponse)
-	log.Printf("[DEBUG] Result: %#v", r)
-	return nil
+	return client, nil
 }
